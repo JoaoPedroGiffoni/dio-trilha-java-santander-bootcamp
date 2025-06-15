9de6a08a52d44b426cd843c9537fb89ea443bcae
joaopedrogiffoni <joaopedrogiffoni@hotmail.com>
2025-06-15 17:30:44 -0300

    Aula final de tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,11 +2,18 @@
 public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
+        // tipos primitivos
         double salarioMinimo = 2500;
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
 
+        int numero1 = 1;
+        numero1 = 2;
+
+        System.err.println(numero1);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
